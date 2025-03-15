c26383ea5d46c96af8b7a4a659c510a48d14ee60
Aayushchauhan570 <aayushchauhan570@gmail.com>
2025-03-15 18:37:52 +0530

    fixed vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,6 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   build: {
-    outDir: 'publish' // Specify the output directory for the build
+    outDir: 'dist' // Change output directory to 'dist'
   }
 });
